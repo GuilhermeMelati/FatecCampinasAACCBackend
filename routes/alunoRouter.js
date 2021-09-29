93820cae1dcc8c299abd41f846ae3ad00c742f65
Pedro Neri <pedrogneri@gmail.com>
2021-09-29 18:32:23 -0300

    adiciona comentários do swagger no alunoRouter

--- routes/alunoRouter.js
+++ routes/alunoRouter.js
@@ -1,166 +1,15 @@
-// REQUIRES NECESSÁRIOS PARA A IMPLEMENTAÇÃO DA ROTA
 const { Router } = require("express");
-const atividadeSchema = require("../models/atividades");
-const statusModificado = require("../utils/notificacoes/statusModificado");
-const alunoRouter = Router();
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
+
+const atividadeSchema = require("../models/atividades");
+const statusModificado = require("../utils/notificacoes/statusModificado");
 const publicKey = fs.readFileSync("./utils/keys/public.key", "utf8");
+
+const alunoRouter = Router();
 require("dotenv/config");
 
-// ROTAS PARA USO DO ALUNO
-
-// OBTER TODAS AS ATIVIDADES DO ALUNO COM BASE NO RA
-alunoRouter.get(
-  "/api/atividades/todas/:RA",
-  verificarJWTAluno,
-  async (req, res, next) => {
-    const RA = req.params.RA;
-    atividadeSchema.find({ RA: RA }, (err, atividades) => {
-      if (!err) {
-        res.status(200).send(atividades);
-      } else {
-        res.status(401).send(err);
-      }
-    });
-  }
-);
-
-// OBTER TODAS AS ATIVIDADES PENDENTES DO ALUNO COM BASE NO RA
-alunoRouter.get("/api/atividades/pendentes/:RA", async (req, res, next) => {
-  const RA = req.params.RA;
-  atividadeSchema.find(
-    {
-      RA: RA,
-      status: "pendente",
-    },
-    (err, atividades) => {
-      if (!err) {
-        res.status(200).send(atividades);
-      } else {
-        res.status(401).send(err);
-      }
-    }
-  );
-});
-
-// OBTER TODAS AS ATIVIDADES CONFIRMADAS DO ALUNO COM BASE NO RA
-alunoRouter.get("/api/atividades/confirmadas/:RA", async (req, res, next) => {
-  const RA = req.params.RA;
-  atividadeSchema.find(
-    {
-      RA: RA,
-      status: "confirmada",
-    },
-    (err, atividades) => {
-      if (!err) {
-        res.status(200).send(atividades);
-      } else {
-        res.status(401).send(err);
-      }
-    }
-  );
-});
-
-// OBTER TODAS AS ATIVIDADES NEGADAS DO ALUNO COM BASE NO RA
-alunoRouter.get(
-  "/api/atividades/negadas/:RA",
-  verificarJWTAluno,
-  async (req, res, next) => {
-    const RA = req.params.RA;
-    atividadeSchema.find(
-      {
-        RA: RA,
-        status: "negada",
-      },
-      (err, atividades) => {
-        if (!err) {
-          res.status(200).send(atividades);
-        } else {
-          res.status(401).send(err);
-        }
-      }
-    );
-  }
-);
-
-// OBTER ATIVIDADE PELO ID
-alunoRouter.get(
-  "/api/atividade/:ID",
-  verificarJWTAluno,
-  async (req, res, next) => {
-    atividadeSchema.findById(req.params.ID, (err, atividade) => {
-      if (!err) {
-        res.status(200).send(atividade);
-      } else {
-        res.status(401).send(err);
-      }
-    });
-  }
-);
-
-// INSERIR UMA NOVA ATIVIDADE
-alunoRouter.post(
-  "/api/atividade",
-  verificarJWTAluno,
-  async (req, res, next) => {
-    const newAtividade = new atividadeSchema(req.body);
-
-    newAtividade.save((err) => {
-      if (!err) {
-        res.status(200).send("Atividade inserida com sucesso!");
-      } else {
-        res.status(401).send(err);
-      }
-    });
-  }
-);
-
-// ATUALIZAR OS DADOS DE UMA ATIVIDADE
-alunoRouter.patch(
-  "/api/atividade",
-  verificarJWTAluno,
-  async (req, res, next) => {
-    const RA = req.body.RA;
-    alunoSchema.findOne({ RA: RA }, (err, aluno) => {
-    if (!err) {
-      if (req.body.status) {
-        statusModificado.enviarEmail(aluno.email, 'localhost:4000', aluno.nome)
-      }
-      atividadeSchema.findByIdAndUpdate(
-        req.body._id,
-        { $set: req.body },
-        (err) => {
-          if (!err) {
-            res.status(200).send("Atividade atualizada com sucesso!");
-          } else {
-            res.status(401).send(err);
-          }
-        }
-      );
-    } else {
-      res.status(401).send(err);
-    }
-  });
-})
-
-// DELETAR UMA ATIVIDADE COM BASE NO ID
-alunoRouter.delete(
-  "/api/atividade/:ID",
-  verificarJWTAluno,
-  async (req, res, next) => {
-    atividadeSchema.findByIdAndRemove(req.params.ID, (err) => {
-      if (!err) {
-        res.status(200).send("Atividade deletada com sucesso!");
-      } else {
-        res.status(401).send(err);
-      }
-    });
-  }
-);
-
-// VER SE O USER ESTÁ LOGADO
-async function verificarJWTAluno(req, res, next) {
+const verificarJWTAluno = async (req, res, next) => {
   var token = req.headers["x-access-token"];
   if (!token) {
     return res.status(401).send({ message: "Token não informado." });
@@ -182,5 +31,203 @@ async function verificarJWTAluno(req, res, next) {
     }
   });
 }
+
+alunoRouter.get("/api/atividades/todas/:RA",
+  verificarJWTAluno, async (req, res) => {
+  /* 
+    #swagger.tags = ['Aluno']
+    #swagger.description = 'Obter todas as atividades do aluno com base no RA'
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }] 
+  */
+
+  const RA = req.params.RA;
+  atividadeSchema.find({ RA: RA }, (err, atividades) => {
+    if (!err) {
+      res.status(200).send(atividades);
+    } else {
+      res.status(401).send(err);
+    }
+  });
+});
+
+alunoRouter.get("/api/atividades/pendentes/:RA",
+  verificarJWTAluno, async (req, res) => {
+  /* 
+    #swagger.tags = ['Aluno']
+    #swagger.description = 'Obter todas as atividades pendentes do aluno com base no RA'
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }] 
+  */
+
+  const RA = req.params.RA;
+  atividadeSchema.find(
+    {
+      RA: RA,
+      status: "pendente",
+    },
+    (err, atividades) => {
+      if (!err) {
+        res.status(200).send(atividades);
+      } else {
+        res.status(401).send(err);
+      }
+    }
+  );
+});
+
+alunoRouter.get("/api/atividades/confirmadas/:RA",
+  verificarJWTAluno, async (req, res) => {
+  /* 
+    #swagger.tags = ['Aluno']
+    #swagger.description = 'Obter todas as atividades confirmadas do aluno com base no RA'
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }] 
+  */
+
+  const RA = req.params.RA;
+  atividadeSchema.find(
+    {
+      RA: RA,
+      status: "confirmada",
+    },
+    (err, atividades) => {
+      if (!err) {
+        res.status(200).send(atividades);
+      } else {
+        res.status(401).send(err);
+      }
+    }
+  );
+});
+
+alunoRouter.get("/api/atividades/negadas/:RA",
+  verificarJWTAluno, async (req, res) => {
+  /* 
+    #swagger.tags = ['Aluno']
+    #swagger.description = 'Obter todas as atividades negadas do aluno com base no RA'
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }] 
+  */
+
+  const RA = req.params.RA;
+  atividadeSchema.find(
+    {
+      RA: RA,
+      status: "negada",
+    },
+    (err, atividades) => {
+      if (!err) {
+        res.status(200).send(atividades);
+      } else {
+        res.status(401).send(err);
+      }
+    }
+  );
+});
+
+alunoRouter.get("/api/atividade/:ID", 
+  verificarJWTAluno, async (req, res) => {
+  /* 
+    #swagger.tags = ['Aluno']
+    #swagger.description = 'Obter atividade pelo id'
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }] 
+  */
+
+  atividadeSchema.findById(req.params.ID, (err, atividade) => {
+    if (!err) {
+      res.status(200).send(atividade);
+    } else {
+      res.status(401).send(err);
+    }
+  });
+});
+
+alunoRouter.post("/api/atividade",
+  verificarJWTAluno, async (req, res) => {
+  /* 
+    #swagger.tags = ['Aluno']
+    #swagger.description = 'Inserir uma nova atividade'
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }]
+    #swagger.parameters['atividade'] = {
+      in: 'body',
+      description: 'Dados da atividade',
+      required: true,
+      type: 'object',
+      schema: {}
+    }
+  */
+
+  const newAtividade = new atividadeSchema(req.body);
+
+  newAtividade.save((err) => {
+    if (!err) {
+      res.status(200).send("Atividade inserida com sucesso!");
+    } else {
+      res.status(401).send(err);
+    }
+  });
+});
+
+alunoRouter.patch("/api/atividade",
+  verificarJWTAluno, async (req, res) => {
+  /* 
+    #swagger.tags = ['Aluno']
+    #swagger.description = 'Atualizar uma atividade'
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }] 
+  */
+
+  const RA = req.body.RA;
+  alunoSchema.findOne({ RA: RA }, (err, aluno) => {
+    if (!err) {
+      if (req.body.status) {
+        statusModificado.enviarEmail(aluno.email, 'localhost:4000', aluno.nome)
+      }
+
+      atividadeSchema.findByIdAndUpdate(
+        req.body._id,
+        { $set: req.body },
+        (err) => {
+          if (!err) {
+            res.status(200).send("Atividade atualizada com sucesso!");
+          } else {
+            res.status(401).send(err);
+          }
+        }
+      );
+    } else {
+      res.status(401).send(err);
+    }
+  });
+})
+
+alunoRouter.delete("/api/atividade/:ID",
+  verificarJWTAluno, async (req, res) => {
+    /* 
+    #swagger.tags = ['Aluno']
+    #swagger.description = 'Deletar uma atividade pelo id'
+    #swagger.security = [{
+      "apiKeyAuth": []
+    }] 
+  */
+
+  atividadeSchema.findByIdAndRemove(req.params.ID, (err) => {
+    if (!err) {
+      res.status(200).send("Atividade deletada com sucesso!");
+    } else {
+      res.status(401).send(err);
+    }
+  });
+});
 
 module.exports = alunoRouter;
