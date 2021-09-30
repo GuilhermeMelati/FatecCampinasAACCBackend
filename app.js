0fb60e01f6bc6d5308ae919a1421e3d4623f24d2
Guilherme Melati <gtmelati@gmail.com>
2021-09-29 22:20:27 -0300

    limiter

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 // REQUIRES NECESSÁRIOS PARA A IMPLEMENTAÇÃO DO SERVIDOR
-const express = require("express");
-const cors = require('cors');
-const mongoose = require("mongoose");
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
+const rateLimit = require('express-rate-limit')
 
-const admRouter = require("./routes/admRouter");
-const alunoRouter = require("./routes/alunoRouter");
-const authRouter = require("./routes/authRouter");
-const professorRouter = require("./routes/professorRouter");
-require("dotenv/config");
+const admRouter = require('./routes/admRouter')
+const alunoRouter = require('./routes/alunoRouter')
+const authRouter = require('./routes/authRouter')
+const professorRouter = require('./routes/professorRouter')
+require('dotenv/config')
+
+// RATE LIMIT
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 30,
+})
 
 // CONECTANDO O MONGO DB ATLAS (REMOTO)
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
 
 // DEFININDO OS SETS DO EXPRESS
-const app = express();
-app.use(cors({
-  origin: '*'
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+const app = express()
+app.use(
+  cors({
+    origin: '*',
+  })
+)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+app.use(limiter)
 
 // ROTAS
 app.use(admRouter)
@@ -32,9 +42,9 @@ app.use(professorRouter)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 // DEFININDO A PORTA DO SERVIDOR (PADRÃO 4000)
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
 // ESCUTANDO A PORTA
 app.listen(port, () => {
-  console.log("Rodando na porta " + port);
-});
+  console.log('Rodando na porta ' + port)
+})
