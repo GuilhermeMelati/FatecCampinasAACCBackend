0aeaa597f0ee71e1d185d6aaf5f4f11272098cce
Guilherme Melati <gtmelati@gmail.com>
2021-10-07 21:12:42 -0300

    formatação dos arquivos e nova rota para recuperar a senha

--- models/alunos.js
+++ models/alunos.js
@@ -1,72 +1,72 @@
 // REQUIRES NECESSÁRIOS PARA A IMPLEMENTAÇÃO DO SCHEMA
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-require("dotenv/config");
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+require('dotenv/config')
 
 // DEFININDO O SCHEMA
 const alunoSchema = new mongoose.Schema({
-  nome: {
-    type: String,
-    required: [true, "Por favor, forneça um nome ao aluno."],
-    maxlength: [60, "Nome do aluno não pode passar de 60 caracteres!"],
-  },
-  RA: {
-    type: String,
-    required: [true, "Por favor, forneça um RA ao aluno."],
-    maxlength: [60, "RA do aluno não pode passar de 60 caracteres!"],
-    index: { unique: true },
-  },
-  email: {
-    type: String,
-    required: [true, "Por favor, forneça um email ao aluno."],
-    maxlength: [60, "Email do aluno não pode passar de 60 caracteres!"],
-    index: { unique: true },
-  },
-  senha: {
-    type: String,
-    required: [true, "Por favor, forneça uma senha ao aluno."],
-    maxlength: [60, "Senha do aluno não pode passar de 60 caracteres!"],
-  },
-  curso: {
-    type: String,
-    required: [true, "Por favor, forneça um curso ao aluno."],
-  },
-  periodo: {
-    type: String,
-    required: [true, "Por favor, forneça um período ao aluno."],
-  },
-  horasAprovadas: {
-    type: Number,
-    required: [
-      true,
-      "Por favor, forneça a quantidade de horas aprovadas do aluno.",
-    ],
-  },
-  horasPendentes: {
-    type: Number,
-    required: [
-      true,
-      "Por favor, forneça a quantidade de horas pendentes do aluno.",
-    ],
-  },
-});
+	nome: {
+		type: String,
+		required: [true, 'Por favor, forneça um nome ao aluno.'],
+		maxlength: [60, 'Nome do aluno não pode passar de 60 caracteres!'],
+	},
+	RA: {
+		type: String,
+		required: [true, 'Por favor, forneça um RA ao aluno.'],
+		maxlength: [60, 'RA do aluno não pode passar de 60 caracteres!'],
+		index: {unique: true},
+	},
+	email: {
+		type: String,
+		required: [true, 'Por favor, forneça um email ao aluno.'],
+		maxlength: [60, 'Email do aluno não pode passar de 60 caracteres!'],
+		index: {unique: true},
+	},
+	senha: {
+		type: String,
+		required: [true, 'Por favor, forneça uma senha ao aluno.'],
+		maxlength: [60, 'Senha do aluno não pode passar de 60 caracteres!'],
+	},
+	curso: {
+		type: String,
+		required: [true, 'Por favor, forneça um curso ao aluno.'],
+	},
+	periodo: {
+		type: String,
+		required: [true, 'Por favor, forneça um período ao aluno.'],
+	},
+	horasAprovadas: {
+		type: Number,
+		required: [
+			true,
+			'Por favor, forneça a quantidade de horas aprovadas do aluno.',
+		],
+	},
+	horasPendentes: {
+		type: Number,
+		required: [
+			true,
+			'Por favor, forneça a quantidade de horas pendentes do aluno.',
+		],
+	},
+})
 
-alunoSchema.pre("save", async function save(next) {
-  if (!this.isModified("senha")) return next();
-  try {
-    const salt = await bcrypt.genSalt(parseInt(process.env.SALT_WORK_FACTOR));
-    this.senha = await bcrypt.hash(this.senha, salt);
-    return next();
-  } catch (err) {
-    return next(err);
-  }
-});
+alunoSchema.pre('save', async function save(next) {
+	if (!this.isModified('senha')) return next()
+	try {
+		const salt = await bcrypt.genSalt(parseInt(process.env.SALT_WORK_FACTOR))
+		this.senha = await bcrypt.hash(this.senha, salt)
+		return next()
+	} catch (err) {
+		return next(err)
+	}
+})
 
 alunoSchema.methods.validarSenha = function (senha, cb) {
-  bcrypt.compare(senha, this.senha, function (err, ok) {
-    if (err) return cb(err);
-    cb(null, ok);
-  });
-};
+	bcrypt.compare(senha, this.senha, function (err, ok) {
+		if (err) return cb(err)
+		cb(null, ok)
+	})
+}
 
-module.exports = mongoose.model("Aluno", alunoSchema);
+module.exports = mongoose.model('Aluno', alunoSchema)
--- models/professor.js
+++ models/professor.js
@@ -1,51 +1,51 @@
 // REQUIRES NECESSÁRIOS PARA A IMPLEMENTAÇÃO DO SCHEMA
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-require("dotenv/config");
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+require('dotenv/config')
 
 // DEFININDO O SCHEMA
 const professorSchema = new mongoose.Schema({
-  nome: {
-    type: String,
-    required: [true, "Por favor, forneça um nome ao professor."],
-    maxlength: [60, "Nome do professor não pode passar de 60 caracteres!"],
-  },
-  email: {
-    type: String,
-    required: [true, "Por favor, forneça um email ao professor."],
-    maxlength: [60, "Email do professor não pode passar de 60 caracteres!"],
-    index: { unique: true },
-  },
-  senha: {
-    type: String,
-    required: [true, "Por favor, forneça uma senha ao professor."],
-    maxlength: [60, "Senha do professor não pode passar de 60 caracteres!"],
-  },
-  superUser: {
-    type: Boolean,
-    required: [
-      true,
-      "Por favor, confirme se o professor será um super usuário do sistema.",
-    ],
-  },
-});
+	nome: {
+		type: String,
+		required: [true, 'Por favor, forneça um nome ao professor.'],
+		maxlength: [60, 'Nome do professor não pode passar de 60 caracteres!'],
+	},
+	email: {
+		type: String,
+		required: [true, 'Por favor, forneça um email ao professor.'],
+		maxlength: [60, 'Email do professor não pode passar de 60 caracteres!'],
+		index: {unique: true},
+	},
+	senha: {
+		type: String,
+		required: [true, 'Por favor, forneça uma senha ao professor.'],
+		maxlength: [60, 'Senha do professor não pode passar de 60 caracteres!'],
+	},
+	superUser: {
+		type: Boolean,
+		required: [
+			true,
+			'Por favor, confirme se o professor será um super usuário do sistema.',
+		],
+	},
+})
 
-professorSchema.pre("save", async function save(next) {
-  if (!this.isModified("senha")) return next();
-  try {
-    const salt = await bcrypt.genSalt(parseInt(process.env.SALT_WORK_FACTOR));
-    this.senha = await bcrypt.hash(this.senha, salt);
-    return next();
-  } catch (err) {
-    return next(err);
-  }
-});
+professorSchema.pre('save', async function save(next) {
+	if (!this.isModified('senha')) return next()
+	try {
+		const salt = await bcrypt.genSalt(parseInt(process.env.SALT_WORK_FACTOR))
+		this.senha = await bcrypt.hash(this.senha, salt)
+		return next()
+	} catch (err) {
+		return next(err)
+	}
+})
 
 professorSchema.methods.validarSenha = function (senha, cb) {
-  bcrypt.compare(senha, this.senha, function (err, ok) {
-    if (err) return cb(err);
-    cb(null, ok);
-  });
-};
+	bcrypt.compare(senha, this.senha, function (err, ok) {
+		if (err) return cb(err)
+		cb(null, ok)
+	})
+}
 
-module.exports = mongoose.model("Professor", professorSchema);
+module.exports = mongoose.model('Professor', professorSchema)
